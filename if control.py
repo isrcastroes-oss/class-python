84a193cd400e868819c75ca9bcff65f8aa4ac6f6
isrcastro.es <isrcastro.es@tecnologicoargos.edu.ec>
2025-09-26 00:50:11 +0000

    primer commit

---
+++ if control.py
@@ -0,0 +1,22 @@
+#if
+edad =20
+
+if edad <13:
+    print ("eres un niño")
+if edad >18:
+    print ("eres un adulto")
+else:
+    prnt ("eres un adulto")
+
+
+edad =17
+tiene_permiso = False
+
+if edad >=18:
+    print ("puedes entrar")
+else:
+    if tiene_permiso:
+        print ("puedes entrar con permiso")
+    else:
+        print ("no puedes entrar")
+    
