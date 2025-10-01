1e679b93920fe2433810dc4ccf9572568206e4cb
isrcastro.es <isrcastro.es@tecnologicoargos.edu.ec>
2025-10-01 00:50:49 +0000

    clase.for

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def main():
-    print("Hello from myapp!")
+nombre = "Juan"
+
+print("nombre: ", nombre)
+
+#interpolaciòn de string
+print(f"nombre: {nombre}")
+
+print(type(nombre))
+
+edad = 25
+print(f"la edad es: {edad}")
+
+altura = 1.75 #float
+
+print(type(altura))
+
+es_estudiante = True
+
+print(type(es_estudiante))a  = 1
+b = 2
+resultado = a + b
+print(resultado)
+
+resultado = a + b
+print(resultado)
+
+resultado = a - b
+print(resultado)
+
+resultado = a * b
+print(resultado)
+
+resultado = a / b
+print(resultado)
 
 
-if __name__ == "__main__":
-    main()
+
+a = 10
+b = 3
+resultado = a // b
+print(resultado)  #3
